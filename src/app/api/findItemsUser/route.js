9bd425a0d07bb99a64635407508821407e38b823
Dan <daniel.speranskiy@live.com>
2024-03-20 14:07:47 +0400

    fixing user items

--- src/app/api/findItemsUser/route.js
+++ src/app/api/findItemsUser/route.js
@@ -7,17 +7,16 @@ export const dynamic = "force-dynamic";
 
 export const GET = async (req) => {
   await connectDB();
-  const email = req.url.split("/").pop().split("=").pop();
+  const email = await req.url.split("/").pop().split("=").pop();
 
   // Find the user by email
   const user = await User.findOne({ email: email });
   const userId = user._id
 console.log(email,'USER EMAIL')
 console.log(userId,'USER ID')
-  // if (!user) {
-  //   // If user not found, return an empty array
-  //   return NextResponse.json([]);
-  // }
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
 
   // Find items associated with the user's _id
   const items = await Item.find({ ref: userId }).sort({ createdAt: -1 });
