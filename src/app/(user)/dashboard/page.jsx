ca29476ac57de5d50ffa631cedd70d894a068239
Dan <daniel.speranskiy@live.com>
2024-03-21 19:10:55 +0400

    added FAQ to pricing page

--- src/app/(user)/dashboard/page.jsx
+++ src/app/(user)/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
 import axios from "axios";
 import {
   Card,
@@ -11,13 +11,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Heading from "@/components/Heading";
-import React, {  useState,useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { fetchUserId } from "@/lib/fetchWeatherData";
-
-
+import { BiSolidZap } from "react-icons/bi";
+import { GoZap } from "react-icons/go";
+import FrequentlyAskedQuestions from "@/components/FrequentlyAskedQuestions";
 
 const DashboardPage = () => {
-const [isPro, setIsPro] = useState(false);
+  const [isPro, setIsPro] = useState(false);
   const { data: session } = useSession();
   const email = session && session.user.email;
 
@@ -35,7 +36,7 @@ const [isPro, setIsPro] = useState(false);
       }
     }
   };
- 
+
   const subscribeUser = async () => {
     try {
       const response = await axios.get("/api/payment?email=" + email);
@@ -47,7 +48,7 @@ const [isPro, setIsPro] = useState(false);
 
   const pricingPlans = [
     {
-      icon: "💸",
+      icon: <GoZap />,
       title: "Free Plan",
       description: "Basic access to the platform",
       features: [
@@ -60,23 +61,20 @@ const [isPro, setIsPro] = useState(false);
       onClick: null, // No action for the free plan
     },
     {
-      icon: "💰",
+      icon: <BiSolidZap />,
       title: "Pro Plan",
       description: "Full features for just $10/month",
-      features: [
-        "Unlimited uploads",
-        "Better suggestions",
-        "Cancel anytime",
-      ],
-      buttonText: 'Subscribe Now',
-      buttonClasses: "bg-blue-500 hover:bg-blue-600 transition duration-300 ease-in-out text-white",
+      features: ["Unlimited uploads", "Better suggestions", "Cancel anytime"],
+      buttonText: "Subscribe Now",
+      buttonClasses:
+        "bg-blue-500 hover:bg-blue-600 transition duration-300 ease-in-out text-white",
       onClick: subscribeUser,
     },
   ];
 
   if (session) {
     return (
-      <div className="w-full h-screen flex flex-col overflow-hidden">
+      <div className="w-full h-full flex flex-col ">
         <Heading
           title={`${session.user?.name}`}
           subTitle="Manage your account"
@@ -84,9 +82,10 @@ const [isPro, setIsPro] = useState(false);
         {isPro && (
           <div className="flex flex-col justify-center items-center">
             <h2 className="text-xl font-semibold tracking-wide text-center">
-              Hey, it seems that you are a Pro member{" "}
+              Hey,{session.user?.name.split(" ")[0]} it seems that you are a Pro
+              user{" "}
             </h2>
-            
+            <img src="/prouser.png" alt="user" className="w-80 h-80 " />
             <button
               onClick={subscribeUser}
               className="bg-blue-500 mt-5 p-2 max-w-sm mx-auto rounded-md hover:bg-blue-600 transition duration-300 ease-in-out text-white"
@@ -96,43 +95,53 @@ const [isPro, setIsPro] = useState(false);
           </div>
         )}
         {!isPro && (
-          <div className="flex flex-col md:flex-row md:justify-around md:space-x-4 p-4">
-            {pricingPlans.map((plan) => (
-              <Card
-                key={plan.title}
-                className="mb-4 md:mb-0 flex flex-col w-full justify-center items-center max-w-md md:max-w-lg mx-auto "
-              >
-                <CardHeader>
-                  <CardTitle className="text-6xl text-center">
-                    {plan.icon}
-                  </CardTitle>
-                  <CardTitle className="text-2xl font-bold tracking-wide text-center">
-                    {plan.title}
-                  </CardTitle>
-                  <CardDescription className="text-center  text-md">
-                    {plan.description}
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="-mt-5">
-                  <h3 className="text-lg font-bold mb-2">Features:</h3>
-                  <ul>
-                    {plan.features.map((feature) => (
-                      <li key={feature}>○ {feature}</li>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardFooter>
-                  <button
-                    onClick={plan.onClick}
-                    className={`${plan.buttonClasses} rounded py-2 px-4`}
-                  >
-                    {plan.buttonText}
-                  </button>
-                </CardFooter>
-              </Card>
-            ))}
-          </div>
+          <>
+            <h3 className="text-3xl font-semibold tracking-wide text-center my-5">
+              Pricing Plans
+            </h3>
+            <div className="flex flex-col md:flex-row md:justify-around md:space-x-4 p-4">
+              {pricingPlans.map((plan) => (
+                <Card
+                  key={plan.title}
+                  className="mb-4 md:mb-0 flex flex-col w-full justify-center items-center max-w-md md:max-w-lg mx-auto "
+                >
+                  <CardHeader>
+                    <CardTitle className="text-6xl text-center flex items-center justify-center">
+                      {plan.icon}
+                    </CardTitle>
+                    <CardTitle className="text-2xl font-bold tracking-wide text-center">
+                      {plan.title}
+                    </CardTitle>
+                    <CardDescription className="text-center  text-md">
+                      {plan.description}
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent className="-mt-5">
+                    <ul>
+                      {plan.features.map((feature) => (
+                        <li key={feature}>✓ {feature}</li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                  <CardFooter>
+                    <button
+                      onClick={plan.onClick}
+                      className={`${plan.buttonClasses} rounded py-2 px-4`}
+                    >
+                      {plan.buttonText}
+                    </button>
+                  </CardFooter>
+                </Card>
+              ))}
+            </div>
+          </>
         )}
+        <h3 className="text-3xl font-semibold tracking-wide text-center my-5">
+          Frequently Asked Questions
+        </h3>
+        <div className="max-w-md md:max-w-xl mx-auto w-full pb-10">
+          <FrequentlyAskedQuestions />
+        </div>
       </div>
     );
   }
---
+++ src/components/FrequentlyAskedQuestions.jsx
@@ -0,0 +1,70 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const FrequentlyAskedQuestions = () => {
+  return (
+    <Accordion type="single" collapsible className="w-full text-start">
+      <AccordionItem value="faq-1">
+        <AccordionTrigger>
+          What subscription plans are available?
+        </AccordionTrigger>
+        <AccordionContent>
+          We offer a variety of plans, including monthly and a free plan
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="faq-2">
+        <AccordionTrigger>
+          Can I change my subscription plan later?
+        </AccordionTrigger>
+        <AccordionContent>
+          Yes, you can upgrade or downgrade your subscription plan at any time
+          through your account settings.
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="faq-3">
+        <AccordionTrigger>Why should I upgrade?</AccordionTrigger>
+        <AccordionContent>
+          Upgrading to a paid plan offers you a greater number of features and
+          unlimited uploads, meaning suggested outfits will be much better.
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="faq-4">
+        <AccordionTrigger>
+          What is included in the free subscription plan?
+        </AccordionTrigger>
+        <AccordionContent>
+          The free plan includes access to basic features with limited usage.
+          For unlimited access, consider upgrading to a premium plan.
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="faq-5">
+        <AccordionTrigger>How do I cancel my subscription?</AccordionTrigger>
+        <AccordionContent>
+          You can cancel your subscription at any time by going to your account
+          settings and selecting 'Manage Subscription'.Your Pro membership will
+          be active until the end of the subscription period.
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="faq-6">
+        <AccordionTrigger className='text-start'>
+          If I cancel my subscription, will I loose my uploads?{" "}
+        </AccordionTrigger>
+        <AccordionContent>
+          Your uploaded items will be kept but you wont be able to upload more
+          unless some are deleted.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default FrequentlyAskedQuestions;
