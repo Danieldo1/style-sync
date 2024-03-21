f0bf8687582db09f66bd346b0e38a24cdc881858
Dan <daniel.speranskiy@live.com>
2024-03-21 19:12:49 +0400

    adding removeBg as edge function

--- src/app/api/removeBg/route.js
+++ src/app/api/removeBg/route.js
@@ -6,7 +6,7 @@ const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN,
 })
 
-
+export const runtime = "edge"
 export const POST = async (req) => {
   const { file } = await req.json();
   const output = await replicate.run(
