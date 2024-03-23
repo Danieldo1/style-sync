85e50d50484b299a53180db3591eb3f098abb203
Dan <daniel.speranskiy@live.com>
2024-03-23 13:50:14 +0400

    added logo to nav landing page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,30 @@
 "use client";
 
 import { useSession, signIn, signOut } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
 import DarkModeSwitchCustom from "./ThemeToggler";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
-
+import  Image  from "next/image";
+import { useTheme } from "next-themes";
 const Nav = () => {
   const { data: session } = useSession();
+  const { theme } = useTheme();
+
 const router = useRouter()
   return (
     <header className="sticky top-0 z-10 bg-muted-foreground/30 py-4 backdrop-filter backdrop-blur-lg border-b border-gray-100">
       <div className="flex items-center container mx-auto justify-between    ">
-        <Link href={"/"} className="text-xl font-bold text1">
-          StyleSync
-        </Link>
+        <div className="flex flex-row items-center justify-center ">
+          <Image
+            src="/logo2.png"
+            alt="Logo"
+            width={35}
+            height={35}
+            className={theme === "dark" ? "" : "invert"}
+          />
+          <p className="text-2xl font-bold text1 -mb-0.5 hidden md:block">tyleSync</p>
+        </div>
         <nav>
           <ul className="flex items-center space-x-4 ">
             <DarkModeSwitchCustom />
@@ -38,9 +47,9 @@ const router = useRouter()
               <>
                 <li>
                   <Button
-                    onClick={() =>
-                      {signIn("google", { callbackUrl: "/dashboard" })}
-                    }
+                    onClick={() => {
+                      signIn("google", { callbackUrl: "/clothes" });
+                    }}
                   >
                     Sign in
                   </Button>
