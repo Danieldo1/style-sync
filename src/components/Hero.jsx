6a71c66138088e9b750e483deb2af670138b852d
Dan <daniel.speranskiy@live.com>
2024-03-27 11:51:31 +0400

    tweaked hero photo margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,11 +11,11 @@ const Hero = () => {
      
     >
       <div className="flex flex-col items-center  justify-center h-full ">
-        <div className="mb-3 mt-3">
-          <h1 className="text-3xl md:text-5xl  font-bold text-center text1">
+        <div className="mb-6 mt-3">
+          <h1 className="text-4xl md:text-5xl  mb-3 font-bold text-center text1 px-3">
             Create your perfect outfit in seconds.
           </h1>
-          <p className="text-lg text-center tracking-wide ">
+          <p className="text-lg text-center tracking-wide px-7 opacity-85">
             Join today to explore your new style, brought to you by your
             clothes, suggested by StyleSync.{" "}
           </p>
@@ -25,7 +25,7 @@ const Hero = () => {
           Trusted by +1400 happy users{" "}
           <span className="text-yellow-500">★★★★★</span>
         </p>
-        <div className="w-full flex justify-center rounded-lg relative h-full mt-2 md:mb-0">
+        <div className="w-full flex justify-center rounded-lg mt-12 relative h-full md:mt-2 md:mb-0">
           <div className="absolute w-40 h-40 z-10 right-[11rem] lg:right-[20rem] xl:right-[30rem] -top-[7rem] -rotate-[110deg] hidden md:block">
             <img src="/arrow.svg" alt="arrow" className="w-full h-full " />
           </div>
